724b43a5bda6bab5490ce5d8b107164015452432
Tiago Valdrich <tiago.valdrich@hotmail.com>
2018-12-02 18:06:32 -0200

    Creating the page for listing all the user orders, and adjusting cart

--- site.php
+++ site.php
@@ -288,7 +288,7 @@
 
       $cart = Cart::getFromSession();
 
-      $totals = $cart->getCalculateTotal();
+      $cart->getCalculateTotal();
 
       $order = new Order();
 
@@ -298,7 +298,7 @@
         'idaddress' => $address->getidaddress(),
         'iduser' => $user->getiduser(),
         'idstatus' => OrderStatus::EM_ABERTO,
-        'vltotal' => $totals['vlprice'] + $cart->getvlfreight()
+        'vltotal' => $cart->getvltotal()
 
       ));
 
@@ -567,6 +567,7 @@
       $taxa_boleto = 5.00;
       $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
       $valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+      $valor_cobrado = str_replace(".", "", $valor_cobrado);
       $valor_cobrado = str_replace(",", ".",$valor_cobrado);
       $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
@@ -625,4 +626,45 @@
 
     });
 
+    $app->get("/profile/orders", function(){
+
+      User::verifylogin(false);
+
+      $user = User::getFromSession();
+
+      $page = new Page();
+
+      $page->setTpl("profile-orders", array(
+
+        'orders' => $user->getOrders()
+
+      ));
+
+    });
+
+    $app->get("/profile/orders/:idorder", function($idorder){
+
+      User::verifylogin(false);
+
+      $order = new Order();
+
+      $order->get((int)$idorder);
+
+      $cart = new Cart();
+
+      $cart->get((int)$order->getidcart());
+
+      $cart->getCalculateTotal();
+
+      $page = new Page();
+
+      $page->setTpl("profile-orders-detail", array(
+
+        'order' => $order->getValues(),
+        'cart' => $cart->getValues(),
+        'products' => $cart->getProducts()
+
+      ));
+
+    });
 ?>
